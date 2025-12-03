6e26eb3093a60b5efb53d45893dbefdb4467e4f2
Ayden Hawkins <aydenhawkins@Aydens-MacBook-Pro.local>
2025-12-02 19:54:34 -0500

    Changed order of header links

--- includes/header.php
+++ includes/header.php
@@ -9,14 +9,14 @@
     <nav>
         <ul>
             <li><a href="index.php" data-nav="home" class="<?php echo (isset($page) && $page === 'home') ? 'active' : ''; ?>">Home</a></li>
+            <li><a href="products.php" data-nav="shop" class="<?php echo (isset($page) && $page === 'shop') ? 'active' : ''; ?>">Shop</a></li>
+            <li><a href="cart.php?action=show_cart" data-nav="cart" class="<?php echo (isset($page) && $page === 'cart') ? 'active' : ''; ?>">Cart<?php if (!empty($_SESSION['cart']) && count($_SESSION['cart']) > 0): ?><span class="cart-badge"><?php echo array_sum(array_column($_SESSION['cart'], 'quantity')); ?></span><?php endif; ?></a></li>
             <?php if (!empty($_SESSION['user_id'])) { ?>
                 <li><a href="logout.php" data-nav="logout">Logout</a></li>
             <?php } else { ?>
                 <li><a href="register.php" data-nav="register" class="<?php echo (isset($page) && $page === 'register') ? 'active' : ''; ?>">Register</a></li>
                 <li><a href="login.php" data-nav="login" class="<?php echo (isset($page) && $page === 'login') ? 'active' : ''; ?>">Login</a></li>
             <?php } ?>
-            <li><a href="products.php" data-nav="shop" class="<?php echo (isset($page) && $page === 'shop') ? 'active' : ''; ?>">Shop</a></li>
-            <li><a href="cart.php?action=show_cart" data-nav="cart" class="<?php echo (isset($page) && $page === 'cart') ? 'active' : ''; ?>">Cart<?php if (!empty($_SESSION['cart']) && count($_SESSION['cart']) > 0): ?><span class="cart-badge"><?php echo array_sum(array_column($_SESSION['cart'], 'quantity')); ?></span><?php endif; ?></a></li>
         </ul>
     </nav>
 </header>
